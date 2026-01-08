3eaa604152b13f8bc884e96b51696e9e93775de7
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2026-01-08 11:57:03 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
-﻿class Hello
-{
-    public static void Main()
-    {
-        System.Console.WriteLine("Hello World");
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Mathmatical Function in C#
+
+int absoluteValue = Math.Abs(-10);
+double squareRoot = Math.Sqrt(16);
+double powerValue = Math.Pow(2, 3);
+int maximumValue = Math.Max(4, 9);
+double roundedValue = Math.Round(4.7);
+
+//displaying result
+Console.WriteLine("Absolute Value: " + absoluteValue);
+Console.WriteLine("Square Root: " + squareRoot);
+Console.WriteLine("Power: " + powerValue);
+Console.WriteLine("Maximum: " + maximumValue);
+Console.WriteLine("Rounded Value: " + roundedValue);
+
+
+Console.ReadLine();
