4a9169e9ccc8a94b48985369cc7d9cf52db1eea7
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2026-01-12 13:52:42 +0530

    Add Program class with Hello World example

---
+++ method.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        NewMethod();
+    }
+
+    public static void NewMethod()
+    {
+        Console.WriteLine("Hello");
+    }
+}
